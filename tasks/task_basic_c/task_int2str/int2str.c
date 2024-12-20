1f3435fa36f1cfef30b2ed31e7351ea4d76c9a16
saiiiek <saiiiek.trifonov@yandex.ru>
2024-12-20 22:17:06 +0100

    Программа проходит все тесты

--- tasks/task_basic_c/task_int2str/int2str.c
+++ tasks/task_basic_c/task_int2str/int2str.c
@@ -1,8 +1,43 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 #include <assert.h>
 #include "int2str.h"
-#include "stdio.h"
+
 
 char* int2str(int number) {
-    char* str = "0";
+    if (number == -2147483648) {
+        char* str = malloc(12 * sizeof(char));
+        assert(str != NULL);
+        snprintf(str, sizeof("-2147483648"), "%s", "-2147483648");
+        return str;
+    }
+
+    int isNegative = (number < 0);
+    if (isNegative) {
+        number = -number;
+    }
+
+    int temp = number;
+    int length = isNegative ? 1 : 0;
+    do {
+        length++;
+        temp /= 10;
+    } while (temp > 0);
+
+    char* str = malloc((length + 1) * sizeof(char));
+    assert(str != NULL);
+    str[length] = '\0';
+
+    int index = length - 1;
+    do {
+        str[index--] = '0' + (number % 10);
+        number /= 10;
+    } while (number > 0);
+
+    if (isNegative) {
+        str[0] = '-';
+    }
+
     return str;
 }
